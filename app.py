112698f070843a85a72926bb6572b5b26168d8ba
dhidot <chandranindhito@students.undip.ac.id>
2022-11-26 22:55:27 +0700

    update routes

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from scipy.spatial import distance
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -32,7 +33,7 @@ def predict():
         model = pickle.load(f)
 
     assignedCluster = []
-    distances = [] # list untuk menampung jarak antara nilai input dengan nilai yang ada di dataset
+    distances = []  # list untuk menampung jarak antara nilai input dengan nilai yang ada di dataset
 
     for i, this_segment in enumerate(model):
         dist = distance.cityblock(vals, this_segment)
@@ -40,8 +41,14 @@ def predict():
         indexMin = np.argmin(distances)
         assignedCluster.append(indexMin)
 
-    return render_template('index.html', resultValue=f'Data yang diinput termasuk kedalam cluster nomor {indexMin, assignedCluster}')
+    if (indexMin == 0):
+        result = 'cluster 1'
+    elif (indexMin == 1):
+        result = 'cluster 2'
+    else:
+        result = 'cluster 3'
 
+    return render_template('predict.html', resultValue=f'Data yang diinput termasuk kedalam {result}')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
--- clustering.py
+++ clustering.py
@@ -7,35 +7,42 @@ import pickle
 
 df = pd.read_csv('transfusion.data')
 
-print(df.head(10))
-print(df.shape)
+# print(df.head(10))
+# print(df.shape)
 
-# Mengubah tipe data atribut class menjadi string
-df['class'] = df['class'].apply(str)
-
-# Ubah value dari class menjadi yes dan no
-df.loc[df["class"] == "1", "class"] = 'yes'
-df.loc[df["class"] == "0", "class"] = 'no'
+# # Mengubah tipe data atribut class menjadi string
+# df['class'] = df['class'].apply(str)
+#
+# # Ubah value dari class menjadi yes dan no
+# df.loc[df["class"] == "1", "class"] = 'yes'
+# df.loc[df["class"] == "0", "class"] = 'no'
 
 x = df[['recency', 'frequency', 'monetary', 'time']]
-print(x.head(10))
+# print(x.head(10))
 
-# Membagi data menjadi data training dan data testing
+# KMeans alqorithm
 kmeans = KMeans(
     n_clusters=3,
     init='k-means++',
     max_iter=300,
     n_init=10,
-    random_state=50,
+    random_state=90,
 )
 
 kfit = kmeans.fit(x)
+# print(kfit.cluster_centers_)
+# print(kfit.labels_)
+# print(kfit.inertia_)
+# print(kfit.n_iter_)
+# print(kfit.n_clusters)
+# print(kfit.n_features_in_)
+# print(kfit.feature_names_in_)
 
 centroids = kmeans.cluster_centers_
 print(centroids)
 print(centroids.shape)
-
-# Menyimpan model ke dalam file pickle secara local
-with open('centroids.pkl', 'wb') as f:
-    pickle.dump(centroids, f)
-    print('Centroids tersimpan ke dalam file pickle')
+#
+# # Menyimpan model ke dalam file pickle secara local
+# with open('centroids.pkl', 'wb') as f:
+#     pickle.dump(centroids, f)
+#     print('Centroids tersimpan ke dalam file pickle')
